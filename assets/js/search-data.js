495e2cdde822e28585c1917d78a13126455de167
arush-gupta <88818461+arush-gupta@users.noreply.github.com>
2025-03-11 06:58:48 +0000

    Deploying to gh-pages from @ arush-gupta/arush-gupta.github.io@4f1cf7dc87e546f8d3001b9a4eb4673102dcb1ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-struggling-in-the-job-market",
+            },{id: "post-",
+      
+        title: "",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/2025-03-11-social-media-job-market/";
+        
+      },
+    },{id: "post-struggling-in-the-job-market",
       
         title: "Struggling in the Job Market",
       
